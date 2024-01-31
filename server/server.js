76949f4352cf0b116a401902a36425ab0a03a666
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2024-01-31 18:38:27 -0500

    startApolloServer(typeDefs,resolvers)

--- server/server.js
+++ server/server.js
@@ -23,7 +23,21 @@ const server = new ApolloServer({
 });
 
 //apply apollo server with express app
-server.applyMiddleware({ app });
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+}
 
 //middleware parsing
 app.use(express.urlencoded({ extended: true }));
